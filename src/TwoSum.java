2d4781089ffdd88fde57bc1d8e76d84362b90394
pwali <pwali@ebay.com>
2020-01-12 10:51:32 -0800

    init

---
+++ src/TwoSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[]{};
+        }
+        int n = nums.length;
+
+        Map<Integer, Integer> idxMap = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            int rem = target - nums[i];
+            if (idxMap.containsKey(rem)) {
+                return new int[] {i, idxMap.get(rem)};
+            }
+            idxMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        ts.twoSum(new int[] {3,2,4,1}, 6);
+    }
+}
